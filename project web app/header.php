52c515e4a7c9f07e1e61e10d371e142720123940
Hamzah Owaidat <124900536+Hamzah-Owaidat@users.noreply.github.com>
2024-06-26 20:48:00 +0300

    Delete project web app directory

--- project web app/header.php
+++
@@ -1,6 +0,0 @@
-<link rel = "stylesheet" href="css/css_header.css">
-<header>
-    <font id="title">Mind Games</font>
-        <div><a href="Home$.php">Home</a></div>    
-        <div><a href="Account.php">Account</a></div>
-</header>
